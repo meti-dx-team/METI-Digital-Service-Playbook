b958a14119b0da2593fe2a72964f355bb9dde309
Taisuke Fukuno <fukuno@jig.jp>
2020-12-31 07:44:29 +0900

    make viewer

--- deno/makecsv.js
+++ deno/makecsv.js
@@ -5,19 +5,19 @@ const s = await Deno.readTextFile("ref.txt");
 let bks = null;
 const list = [];
 for (const ss of s.split("\n")) {
-  bks = ss;
   const url = ss;
   if (!url.startsWith("https://") && !url.startsWith("http://")) {
+    bks = ss;
     continue;
   }
   if (url.endsWith(".pdf")) {
-    list.push({ url, title: bks });
+    list.push({ title: bks, url });
   } else {
     const html = await (await fetch(url)).text();
     const dom = cheerio.load(html);
     const title = dom("title").text();
     console.log(url, title);
-    list.push({ url, title });
+    list.push({ title, url });
   }
 }
 await Deno.writeTextFile("../reference.csv", CSV.encode(CSV.fromJSON(list)));
